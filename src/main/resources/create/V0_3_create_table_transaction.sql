3b64eeb06f10b45fbe40814d527599ab00f7097e
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2023-12-06 16:31:11 +0300

    fix: debuging in the script sql

--- src/main/resources/create/V0_3_create_table_transaction.sql
+++ src/main/resources/create/V0_3_create_table_transaction.sql
@@ -11,10 +11,9 @@ $$
 $$;
 CREATE TABLE IF NOT EXISTS "transaction" (
     id            varchar               constraint transaction_pk primary key default uuid_generate_v4(),
-    account_id          VARCHAR         NOT NULL REFERENCES "accounts"("id"),
     label               label           not null,
     type                type            not null,
-    amount              DOUBLE          NOT NULL,
+    amount              DECIMAL(10, 2)          NOT NULL,
     transaction_date    TIMESTAMP       DEFAULT CURRENT_TIMESTAMP
     );
     create index if not exists amount_index on "transaction" (amount);
