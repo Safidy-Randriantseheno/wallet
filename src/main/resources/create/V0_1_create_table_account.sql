a905c43922fdce7a3dbef607266f21fcbf608341
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2023-12-06 11:14:08 +0300

    chore: create table balance and connect with account

--- src/main/resources/create/V0_1_create_table_account.sql
+++ src/main/resources/create/V0_1_create_table_account.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS "accounts" (
     id            varchar
                               constraint accounts_pk primary key default uuid_generate_v4(),
     name VARCHAR(255) NOT NULL,
-    devise_id VARCHAR NOT NULL REFERENCES "devise"("id")
+    devise_id VARCHAR NOT NULL REFERENCES "devise"("id"),
+    balance_id VARCHAR NOT NULL REFERENCES "balance"("id")
 );
 create index if not exists accounts_name_index on "accounts" (name);
