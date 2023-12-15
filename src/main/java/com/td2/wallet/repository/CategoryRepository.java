0c15fec32b6bd3f8b1f81bd33d1195571a448378
Mitsanta_ANDRIAH <hei.mitsanta@gmail.com>
2023-12-15 16:37:47 +0300

    feat: add function to calculateCategorySumBetweenDates

--- src/main/java/com/td2/wallet/repository/CategoryRepository.java
+++ src/main/java/com/td2/wallet/repository/CategoryRepository.java
@@ -3,12 +3,17 @@ package com.td2.wallet.repository;
 import com.td2.wallet.model.Category;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 @AllArgsConstructor
@@ -31,6 +36,42 @@ public class CategoryRepository {
             return null;
         }
     }
+    public BigDecimal calculateCategorySumBetweenDates(String accountId, LocalDateTime startDate, LocalDateTime endDate) {
+        BigDecimal restaurantSum = BigDecimal.ZERO;
+        BigDecimal salaireSum = BigDecimal.ZERO;
 
+        // Requête pour récupérer les montants et noms des catégories pour un compte et une dates donnés
+        try {
+            String sql = "SELECT t.amount, c.name " +
+                    "FROM transaction t " +
+                    "LEFT JOIN category c ON t.category_id = c.id " +
+                    "WHERE t.account_id = ? " +
+                    "AND t.transaction_date >= ? " +
+                    "AND t.transaction_date <= ?";
+
+            // Exécution de la requête et stockage des résultats dans une liste de maps
+            List<Map<String, Object>> rows = jdbcTemplate.queryForList(
+                    sql, accountId, startDate, endDate);
+
+            // Parcourt chaque ligne des resultats obtenue
+            for (Map<String, Object> row : rows) {
+                // Récupère le nom de la catégorie et le montant de la transaction depuis chaque ligne
+                String categoryName = (String) row.get("name");
+                BigDecimal amount = (BigDecimal) row.get("amount");
+
+                // Incrémente la somme pour la catégorie 'Restaurant' ou 'Salaire'
+                if ("Restaurant".equals(categoryName)) {
+                    restaurantSum = restaurantSum.add(amount);
+                } else if ("Salaire".equals(categoryName)) {
+                    salaireSum = salaireSum.add(amount);
+                }
+            }
+        } catch (DataAccessException e) {
+            // Gére les exceptions
+            e.printStackTrace();
+        }
+        // Retourne la somme totale des montants pour 'Restaurant' et 'Salaire'
+        return restaurantSum.add(salaireSum);
+    }
 
 }
