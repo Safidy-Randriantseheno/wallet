7773d28153985d3b0829df76e4acba9bfd86bd4b
Mitsanta_ANDRIAH <hei.mitsanta@gmail.com>
2023-12-15 14:34:41 +0300

    first commit

--- src/main/java/com/td2/wallet/WalletApplication.java
+++ src/main/java/com/td2/wallet/WalletApplication.java
@@ -1,16 +1,9 @@
 package com.td2.wallet;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class WalletApplication {
